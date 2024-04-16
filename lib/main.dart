0032e51deff3b3a0097b552a46b229c8f8abb0fe
Ahmed Samir <ahmedsami.r@yahoo.com>
2024-04-16 02:02:39 +0200

    frist commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FashionTown());
+}
+
+class FashionTown extends StatelessWidget {
+  const FashionTown({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fashion Town',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
